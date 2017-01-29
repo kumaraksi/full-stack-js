cdd132e81b45156686b464066999bccf13534972
akshayk <akshayk@cisco.com>
2017-01-29 21:32:44 +0530

    Project Setup

---
+++ src/client/app.jsx
@@ -0,0 +1,19 @@
+import 'babel-polyfill';
+
+import React, { PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+import Dog from '../shared/dog';
+
+const dogBark = new Dog('React Toby').bark();
+
+const App = props => (
+  <div>
+    the dog says: {props.message}
+  </div>
+);
+
+App.propTypes = {
+  message: PropTypes.string.isRequired,
+};
+
+ReactDOM.render(<App message={dogBark} />, document.querySelector('.app'));
